e5f1fa5c537af493557fe0606b97204f05aeecb5
zeelu07 <pzeel0452@gmail.com>
2026-03-03 11:03:11 +0530

    3-3

---
+++ sring.py
@@ -0,0 +1,18 @@
+s="Tops Technologies"
+
+print(s)
+print(s.capitalize())
+print(s.casefold())
+print(s.lower())
+print(s.upper())
+print(s.center(40,"*"))
+print(s.endswith("ese"))
+print(s.startswith("To"))
+print(s.find("log"))
+print(s.index("T",2))
+print("tops123".isalnum())
+print("tops".isalnum())
+print("123".isalnum())
+print("tops".isalpha())
+print("123".isnumeric())
+print(s.istitle())
---
+++ sringslice.py
@@ -0,0 +1,17 @@
+#01234567890123456    +
+s="Tops Technologies"
+#76543210987654321    _
+
+
+print(s[3:15])
+print(s[:13])
+print(s[2:])
+print(s[2:14:3])
+print(s[::5])
+
+
+print(s[-15:-3])
+print(s[:-2])
+print(s[-14:])
+print(s[-16:-1:4])
+print(s[::-1])
